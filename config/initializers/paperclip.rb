4722219f096d3098f81ed55267ccdf54458f7a6c
Anton Khaybulaev <ant.khay@gmail.com>
2015-08-07 03:18:07 +0600

    fixing s3

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,2 @@
-Paperclip::Attachment.default_options[:path] = '/plasttara/:class/:attachment/:id_partition/:style/:filename'
+Paperclip::Attachment.default_options[:path] = '/plast-tara/:class/:attachment/:id_partition/:style/:filename'
 Paperclip::Attachment.default_options[:s3_host_name] = 's3-eu-central-1.amazonaws.com'
