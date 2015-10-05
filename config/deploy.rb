85cbf7b2113f25c6f09144fc39443ab3dc5fdc6c
Anton Khaybulaev <ant.khay@gmail.com>
2015-10-06 01:26:45 +0600

    Setup for locum

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,8 +16,7 @@ set :unicorn_pid,    "/var/run/unicorn/#{fetch(:user)}/" \
 set :bundle_without, [:development, :test]
 set :use_sudo,       false
 
-set :repo_url,       "#{fetch(:user)}@calcium.locum.ru:" \
-                     "git/#{fetch(:application)}.git"
+set :repo_url,       "git@bitbucket.org:elmirill/plastic_tare.git"
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
