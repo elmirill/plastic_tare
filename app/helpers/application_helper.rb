698b4f3150ee1f76ad2987d5309703be24eecb26
Anton Khaybulaev <ant.khay@gmail.com>
2015-10-29 16:30:22 +0600

    Implementing meta

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,19 @@
 module ApplicationHelper
+	
+	def meta_title(meta_title = nil)
+		if meta_title.present?
+			content_for :meta_title, meta_title
+		else
+			content_for?(:meta_title) ? content_for(:meta_title) + ' | ' + ENV['default_meta_title'] : ENV['default_meta_title']
+		end
+	end
+	
+	def meta_description(meta_description = nil)
+		if meta_description.present?
+			content_for :meta_description, meta_description
+		else
+			content_for?(:meta_description) ? content_for(:meta_description) : ENV['default_meta_description']
+		end
+	end
 
 end
